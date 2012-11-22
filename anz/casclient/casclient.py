5bc32e49cbf3602d063fcc7e263cc560971cf42e
Victor Fernandez de Alba <sneridagh@gmail.com>
2012-11-22 16:32:19 +0100

    Whitespace

--- anz/casclient/casclient.py
+++ anz/casclient/casclient.py
@@ -39,7 +39,7 @@ def manage_addAnzCASClient( self, id, title=None, REQUEST=None ):
     ''' Add an instance of anz cas client to PAS. '''
     obj = AnzCASClient( id, title )
     self._setObject( obj.getId(), obj )
-    
+
     if REQUEST is not None:
         REQUEST['RESPONSE'].redirect(
             '%s/manage_workspace'
@@ -50,33 +50,33 @@ def manage_addAnzCASClient( self, id, title=None, REQUEST=None ):
 
 class AnzCASClient( BasePlugin, Cacheable ):
     ''' Anz CAS client, Implement as a PAS plugin. '''
-    
+
     implements( IAnzCASClient )
-    
+
     meta_type = 'Anz CAS Client'
-    
+
     # Session variable use to save assertion
     CAS_ASSERTION = '__cas_assertion'
-    
+
     # The start of the CAS server URL
     casServerUrlPrefix = ''
-    
+
     # An identify of current service.
     # CAS will redirects to here after login.
     # Set this explicitly but not determine it automatically from request makes
     # us get more security assurance.
     # https://wiki.jasig.org/display/CASC/CASFilter
     serviceUrl = ''
-    
+
     # Whether to store the Assertion in session or not.
     # If sessions are not used, proxy granting ticket will be required for
     # each request. Default set to True.
     useSession = True
-    
+
     # If set to True, CAS will ask user for credentials again to authenticate,
     # this may be used for high-security applications. Default set to False.
     renew = False
-    
+
     # If set to True, CAS will not ask the user for credentials. If the
     # user has a pre-existing single sign-on session with CAS, or if a single
     # sign-on session can be established through non-interactive means
@@ -94,34 +94,34 @@ class AnzCASClient( BasePlugin, Cacheable ):
     # is undefined if both are set to True.
     # See details here: http://www.jasig.org/cas/client-integration/gateway
     gateway = False
-    
+
     # Use which CAS protocol to validate ticket
     # one of ['CAS 1.0','CAS 2.0']
     ticketValidationSpecification = 'CAS 1.0'
     ticketValidationSpecification_values = ['CAS 1.0','CAS 2.0']
-    
+
     # The start of the proxy callback url.
-    # You should set it point to an instance of this class with protocol 'https'. 
+    # You should set it point to an instance of this class with protocol 'https'.
     # The result url will be '{proxyCallbackUrlPrefix}/proxyCallback'.
     # If set, means this service will be used as a proxier to access
     # back-end service on behalf of a particular user.
     proxyCallbackUrlPrefix = ''
-    
+
     # If you provide either the acceptAnyProxy or the allowedProxyChains
     # parameters, a Cas20ProxyTicketValidator will be constructed. Otherwise
     # a Cas20ServiceTicketValidator will be constructed that does not accept
     # proxy tickets.
-    
+
     # Whether any proxy is OK
     acceptAnyProxy = False
-    
+
     # Allowed proxy chains.
     # Each acceptable proxy chain should include a space-separated list of URLs.
     # These URLs are proxier's proxyCallbackUrl.
     allowedProxyChains = []
-    
+
     security = ClassSecurityInfo()
-    
+
     _properties = (
         {
             'id': 'serviceUrl',
@@ -179,27 +179,27 @@ class AnzCASClient( BasePlugin, Cacheable ):
             'mode': 'w'
             },
         )
-    
+
     def __init__( self, id, title ):
         self._id = self.id = id
         self.title = title
         self._pgtStorage = ProxyGrantingTicketStorage()
         self._sessionStorage = SessionMappingStorage()
-    
+
     security.declarePrivate( 'extractCredentials' )
     def extractCredentials( self, request ):
         ''' Extract credentials from session or 'request'. '''
         creds = {}
-        
+
         # Do logout if logout request found
         logoutRequest = request.form.get( 'logoutRequest', '' )
         if logoutRequest:
             self.logoutCallback()
             return creds
-        
+
         sdm = getattr( self, 'session_data_manager', None )
         assert sdm is not None, 'No session data manager found!'
-        
+
         session = sdm.getSessionData( create=0 )
         assertion = self.getAssertion( session )
         if not assertion:
@@ -207,18 +207,18 @@ class AnzCASClient( BasePlugin, Cacheable ):
             ticket = request.form.get( 'ticket', None )
             if not ticket:
                 return None # No CAS authentification
-            
+
             service = self.getService()
             assertion = self.validateServiceTicket( service, ticket )
-            
+
             # Save current user's session to be used by 'Single Sign Out'
             if not session:
                 session = sdm.getSessionData( create=1 )
-            
+
             # Get session token as id, it is more reliable
             sessionId = session.getContainerKey()
             self._sessionStorage.addSession( ticket, sessionId )
-            
+
             # Save assertion into session
             if self.useSession:
                 # During ticket validation process, the server with this client
@@ -227,25 +227,25 @@ class AnzCASClient( BasePlugin, Cacheable ):
                 # we get it again.
                 session = sdm.getSessionData()
                 session.set( self.CAS_ASSERTION, assertion )
-        
+
         creds['login'] = assertion.getPrincipal().getId()
         return creds
-    
+
     security.declarePrivate( 'authenticateCredentials' )
     def authenticateCredentials( self, credentials ):
         if credentials['extractor'] != self.getId():
             return None
-        
+
         login = credentials['login']
         return ( login, login )
-    
+
     security.declarePrivate( 'challenge' )
     def challenge( self, request, response, **kw ):
         ''' Challenge the user for credentials. '''
         # Remove current credentials.
         session = request.SESSION
         session[self.CAS_ASSERTION] = None
-        
+
         # Redirect to CAS login URL.
         if self.casServerUrlPrefix:
             url = self.getLoginURL() + '?service=' + self.getService()
@@ -253,22 +253,22 @@ class AnzCASClient( BasePlugin, Cacheable ):
                 url += '&renew=true'
             if self.gateway:
                 url += '&gateway=true'
-            
+
             response.redirect( url, lock=1 )
             return 1
-        
+
         # Fall through to the standard unauthorized() call.
         return 0
-    
+
     security.declarePrivate( 'resetCredentials' )
     def resetCredentials( self, request, response ):
         ''' Clears credentials and redirects to CAS logout page. '''
         session = request.SESSION
         session.clear()
-        
+
         if self.casServerUrlPrefix:
             return response.redirect( self.getLogoutURL(), lock=1 )
-    
+
     security.declarePublic( 'proxyCallback' )
     def proxyCallback( self, pgtId=None, pgtIou=None ):
         ''' See interfaces.IAnzCASClient. '''
@@ -277,9 +277,9 @@ class AnzCASClient( BasePlugin, Cacheable ):
             self._pgtStorage.add( pgtIou, pgtId )
             ret = '<?xml version=\"1.0\"?>'
             ret += '<casClient:proxySuccess xmlns:casClient="http://www.yale.edu/tp/casClient" />'
-        
+
         return ret
-    
+
     security.declarePublic( 'logoutCallback' )
     def logoutCallback( self ):
         ''' See interfaces.IAnzCASClient. '''
@@ -292,21 +292,21 @@ class AnzCASClient( BasePlugin, Cacheable ):
             mappingId = elements[0].firstChild.data
             sessionId = self._sessionStorage.getSessionId( mappingId )
             self._sessionStorage.removeByMappingId( mappingId )
-            
+
             sdm = getattr( self, 'session_data_manager', None )
             assert sdm is not None, 'No session data manager found!'
-            
+
             session = sdm.getSessionDataByKey( sessionId )
             if session:
                 session.clear()
-                
+
                 # We must commit here to make sure the session will be cleared.
                 transaction.commit()
-            
+
             msg = 'Logout seccess.'
-        
+
         return msg
-    
+
     security.declarePublic( 'validateProxyTicket' )
     def validateProxyTicket( self, ticket ):
         ''' See interfaces.IAnzCASClient. '''
@@ -316,7 +316,7 @@ class AnzCASClient( BasePlugin, Cacheable ):
             acceptAnyProxy=self.acceptAnyProxy,
             allowedProxyChains=self.allowedProxyChains,
             renew=self.renew )
-        
+
         try:
             assertion = validator.validate( ticket, self.getService() )
         except BaseException, e:
@@ -327,53 +327,53 @@ class AnzCASClient( BasePlugin, Cacheable ):
             return False, None
         else:
             return True, assertion
-    
+
     def getLoginURL( self ):
         ''' See interfaces.IAnzCASClient. '''
         return self.casServerUrlPrefix + '/login'
-    
+
     def getLogoutURL( self ):
         ''' See interfaces.IAnzCASClient. '''
         return self.casServerUrlPrefix + '/logout?url=%s' % self.getService()
-    
+
     def getService( self ):
         ''' See interfaces.IAnzCASClient. '''
         serviceUrl = ''
-        
+
         if self.serviceUrl:
             # use explicitly setted service url
             serviceUrl = self.serviceUrl
         else:
             # extract service URL from REQUEST and remove the ticket parameters.
             request = self.REQUEST
-            
+
             # get query string but strip ticket parameter
             query_string = request.get( 'QUERY_STRING', '' )
             parts = [ p for p in query_string.split('&') if \
                       (p and p.find( 'ticket=' )!=0) ]
-            
+
             url = request.get( 'ACTUAL_URL', request['URL'] )
-            
+
             serviceUrl = '%s?%s' % ( url, '&'.join(parts) )
-        
+
         return quote( serviceUrl )
-    
+
     def getProxyCallbackUrl( self ):
         ''' See interfaces.IAnzCASClient. '''
         return self.proxyCallbackUrlPrefix and \
                '%s/proxyCallback' % self.proxyCallbackUrlPrefix or ''
-    
+
     def getAssertion( self, session ):
         ''' See interfaces.IAnzCASClient. '''
         assertion = None
-        
+
         if self.useSession and session:
             sessionValue = session.get( self.CAS_ASSERTION )
             if isinstance( sessionValue, Assertion ):
                 assertion = sessionValue
-        
+
         return assertion
-    
+
     def validateServiceTicket( self, service, ticket ):
         ''' See interfaces.IAnzCASClient. '''
         if self.ticketValidationSpecification == 'CAS 1.0':
@@ -390,14 +390,14 @@ class AnzCASClient( BasePlugin, Cacheable ):
             else:
                 validator = Cas20ServiceTicketValidator(
                     self.casServerUrlPrefix, self._pgtStorage, self.renew )
-        
+
         return validator.validate(
             ticket, service, self.getProxyCallbackUrl() )
 
-classImplements( AnzCASClient, 
-                 IExtractionPlugin, 
-                 IChallengePlugin, 
-                 ICredentialsResetPlugin, 
+classImplements( AnzCASClient,
+                 IExtractionPlugin,
+                 IChallengePlugin,
+                 ICredentialsResetPlugin,
                  IAuthenticationPlugin )
 
 InitializeClass( AnzCASClient )
